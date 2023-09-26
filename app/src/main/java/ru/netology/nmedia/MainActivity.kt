c51145a603fb36d25fd8c3cc0645555e57bd8b2e
Maria Bulashova <mm.bulashova@gmail.com>
2023-09-26 15:45:54 +0300

    4. Like, Share

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,49 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Count
+import ru.netology.nmedia.dto.Post
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            likedByMe = false,
+            likes = 6_099,
+            shares = 9_999,
+            views = 1_332_999
+        )
+        with(binding) {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            likeCount.text = Count.formatCount(post.likes)
+            shareCount.text = Count.formatCount(post.shares)
+            viewCount.text = Count.formatCount(post.views)
+
+            if (post.likedByMe) {
+                like.setImageResource(R.drawable.ic_liked_24)
+            }
+
+            like.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+                post.likes += if (post.likedByMe) 1 else -1
+                like.setImageResource(if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24)
+                likeCount.text = Count.formatCount(post.likes)
+            }
+
+            share.setOnClickListener {
+                post.shares++
+                shareCount.text = Count.formatCount(post.shares)
+            }
+        }
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/dto/Count.kt
@@ -0,0 +1,13 @@
+package ru.netology.nmedia.dto
+
+import kotlin.math.truncate
+
+object Count {
+    fun formatCount(count: Int): String =
+        when {
+            count in 1_000..9_999 -> if (count % 1000 >= 100) "${truncate((count / 100).toDouble()) / 10.0}K" else "${count / 1_000}K"
+            count in 10_000..999_999 -> "${count / 1_000}K"
+            count >= 1_000_000 -> if (count % 1_000_000 >= 100_000) "${truncate((count / 100_000).toDouble()) / 10.0}M" else "${count / 1_000_000}M"
+            else -> count.toString()
+        }
+}
