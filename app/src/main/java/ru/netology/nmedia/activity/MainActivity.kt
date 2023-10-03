0ad7b970a1233d628c323d0648bfad6dfb804001
Maria Bulashova <mm.bulashova@gmail.com>
2023-10-03 18:24:25 +0300

    5. MVVM

---
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -0,0 +1,38 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel by viewModels<PostViewModel>()
+
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
+                likeCount.text = Count.formatCount(post.likes)
+                shareCount.text = Count.formatCount(post.shares)
+                viewCount.text = Count.formatCount(post.views)
+                like.setImageResource(if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24)
+            }
+        }
+
+        binding.like.setOnClickListener {
+            viewModel.like()
+        }
+
+        binding.share.setOnClickListener {
+            viewModel.share()
+        }
+    }
+}
