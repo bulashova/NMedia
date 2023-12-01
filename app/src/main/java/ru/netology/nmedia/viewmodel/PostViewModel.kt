3f3ee550ebe226969e62e694ec4b7d069384867d
Maria Bulashova <mm.bulashova@gmail.com>
2023-12-01 18:12:55 +0300

    10. Storage

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,10 +1,11 @@
 package ru.netology.nmedia.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositoryFilesImpl
 
 private val empty = Post(
     id = 0,
@@ -14,9 +15,9 @@ private val empty = Post(
     published = ""
 )
 
-class PostViewModel : ViewModel() {
+class PostViewModel (application: Application): AndroidViewModel(application) {
 
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    private val repository: PostRepository = PostRepositoryFilesImpl(application)
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
 
